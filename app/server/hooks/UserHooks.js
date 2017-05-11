c05a774f478ba1ca553bc71855dc00f579cfdaf6
Alexandre Hanot <alexandrehanot@MacBook-Pro-de-Alexandre.local>
2017-05-11 01:26:13 -0400

    bugfix

--- app/server/hooks/UserHooks.js
+++ app/server/hooks/UserHooks.js
@@ -1,7 +1,7 @@
 Meteor.users.after.update(function (userId, doc, fieldNames, modifier, options) {
 	console.log(modifier);
 
-	if(modifier.$addToSet['private.requests']){
+	if(modifier && modifier.$addToSet && modifier.$addToSet['private.requests']){
 		let request = modifier.$addToSet['private.requests'];
 		if(_.contains(Meteor.users.findOne(request).requests(), userId))
 			Conversation.insert({
