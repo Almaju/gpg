185b15116e092690e38d057366518578036c5cef
Alexandre Hanot <alexandrehanot@macbook-pro-de-alexandre-jpc.home>
2018-08-16 15:56:57 +0200

    healthcheck

--- app/server/config/startup.js
+++ app/server/config/startup.js
@@ -2,7 +2,7 @@
 // 	ServiceConfiguration.configurations.update({"service": "facebook" }, {
 // 		$set: {
 // 			"appId": "186082101899198",
-// 			"secret": "3932eb3001643b0d3725041d6cdd6ae5"
+// 			"secret": ""
 // 		}
 // 	},{
 // 		upsert: true
@@ -11,7 +11,7 @@
 // 		ServiceConfiguration.configurations.update({"service": "google" }, {
 // 		$set: {
 // 			"clientId": "1077104954992-5rflo0f3ecurvncecgvis8gkb5ld4koe.apps.googleusercontent.com",
-// 			"secret": "_XbfgJja254kJS4O9Bl6YpiR"
+// 			"secret": ""
 // 		}
 // 	},{
 // 		upsert: true
